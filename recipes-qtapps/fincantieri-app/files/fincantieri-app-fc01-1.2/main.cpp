f6a5d95844355d3a03c6db504fee1b24a74da047
Giulio Marescotti <giulio.marescotti@rgm.it>
2022-06-23 10:28:57 +0000

    feat: added parses option to skip timer

--- recipes-qtapps/fincantieri-app/files/fincantieri-app-fc01-1.2/main.cpp
+++ recipes-qtapps/fincantieri-app/files/fincantieri-app-fc01-1.2/main.cpp
@@ -1,4 +1,7 @@
+#include <QCommandLineOption>
+#include <QCommandLineParser>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
 
 #if defined(Q_OS_WIN)
 #include <QGuiApplication>
@@ -19,7 +22,24 @@ int main(int argc, char *argv[])
     QString qrc_file = QStringLiteral("qrc:/main.qml");
 #endif
 
+    app.setApplicationName("fincantieri-app-fc01");
+    app.setApplicationVersion("1.0");
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QCoreApplication::translate("main", "Gateway between MODBUS and CANBUS packets"));
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    QCommandLineOption noTimerOption(QStringList() << "n" << "no-timer", QCoreApplication::translate("main", "Don't send polling CAN packets (skip Timer)."));
+    parser.addOption(noTimerOption);
+    parser.process(app);
+
     QQmlApplicationEngine engine;
+
+    QVariant &skip_timer = QVariant::fromValue(parser.isSet(noTimerOption));
+
+    QQmlContext *context = new QQmlContext(engine.rootContext());
+    context->setContextProperty("skip_timer", &skip_timer);
 
 #if defined(Q_OS_WIN)
     cantools_qt_init(&engine, "ixxatcan", "", 500000);
