41247c04842d621c8e71c01b126c6a6dc1ee4aae
Giulio Marescotti <giulio.marescotti@rgm.it>
2022-08-03 09:54:24 +0000

    chore: removed binary files ops

--- recipes-qtapps/fincantieri-app/files/build-fincantieri_app-Desktop_Qt_5_15_2_MSVC2019_64bit-Debug/cantools_qt_init.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "mycan.h"
-#include <QQmlApplicationEngine>
-
-MyCan *cantools_qt_init(QQmlApplicationEngine *engine, QString plugin, QString device, quint32 bitrate, const int sniffer_max_size=1000);
--- recipes-qtapps/fincantieri-app/files/build-fincantieri_app-Desktop_Qt_5_15_2_MSVC2019_64bit-Debug/modbus_qt_init.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "mymodbus.h"
-#include <QQmlApplicationEngine>
-
-MyModbus *modbus_qt_init(QQmlApplicationEngine *engine, QString ip="");
